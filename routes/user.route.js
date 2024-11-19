716a940c738641e662fa2eed6d13c34e81ea1efe
IsaacNdulue <Izikmessiah@gmail.com>
2024-11-19 19:21:35 +0100

    structuring

--- routes/user.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createUser } from "../controllers/userControl";
-const router = express.Router();
-
-// Define routes here
-router.post("/create", createUser);
-
-export default router;
